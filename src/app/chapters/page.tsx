763e2889818e8e22a3a030bbf9921d0d61ec7d47
PaulWallaceATL <Paul@tryclinixai.com>
2025-09-28 04:02:52 -0400

    fix(chapters): import useSearchParams from next/navigation

--- src/app/chapters/page.tsx
+++ src/app/chapters/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import Header from '@/components/Header';
